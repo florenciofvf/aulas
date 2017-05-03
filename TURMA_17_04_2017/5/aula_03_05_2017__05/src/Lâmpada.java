a4fa88acb7dce7994889401160d793aa21682079
Florêncio Vieira Filho <florenciovieira@gmail.com>
2017-05-03 20:01:50 -0300

    Aula 05

---
+++ TURMA_17_04_2017/5/aula_03_05_2017__05/src/Lâmpada.java
@@ -0,0 +1,22 @@
+class Lâmpada 
+  {
+      short voltagem;
+      
+      void acender()
+        {
+           voltagem = 220;
+        }
+        
+      void apagar()
+        {
+           voltagem = 0;
+        }  
+        
+      void mostrarEstado()
+        {
+           if ( voltagem == 220 )
+              System.out.println( "ACESA" );
+           else
+              System.out.println( "APAGADA" );
+        }  
+  }
