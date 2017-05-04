bde4a94abcd94c139d2fd580f95da75a5d202324
Florêncio Vieira Filho <florenciovieira@gmail.com>
2017-05-03 22:03:55 -0300

    Classe Televisao

--- TURMA_17_04_2017/5/aula_03_05_2017__05/src/ContaSimples.java
+++ TURMA_17_04_2017/5/aula_03_05_2017__05/src/ContaSimples.java
@@ -1,6 +1,7 @@
 
 public class ContaSimples {
 	private float saldo;
+	private float limite;
 	
 	public void depositar(float valor) {
 		if(valor > 0) {
@@ -8,9 +9,13 @@ public class ContaSimples {
 		}
 	}
 
+	public void tornarEspecial() {
+		limite = 1000;
+	}
+	
 	public float sacar(float valor) {
 		
-		if(valor <= saldo) {
+		if(valor <= saldo + limite) {
 			saldo -= valor;
 			return valor;
 		}
@@ -21,5 +26,10 @@ public class ContaSimples {
 	public void mostrarDadosDaConta() {
 		System.out.println("====(DADOS DA CONTA)====");
 		System.out.printf("SALDO R$ %.2f \n", saldo);
+		if(limite > 0) {
+			System.out.println("CONTA ESPECIAL");
+			System.out.printf("LIMITE R$ %.2f \n", limite);
+			System.out.printf("DISPONÍVEL PARA SAQUE R$ %.2f \n", saldo + limite);
+		}
 	}
 }
