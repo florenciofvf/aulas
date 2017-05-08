9b0fcdbda4eaa12b79fc1eb144df90dda5e66c0d
Florêncio Vieira Filho <florenciovieira@gmail.com>
2017-05-08 19:25:37 -0300

    Construtores

---
+++ TURMA_17_04_2017/7/aula_08_05_2017__07/src/br/com/turma_java/TesteUnidade.java
@@ -0,0 +1,11 @@
+package br.com.turma_java;
+
+public class TesteUnidade {
+	public static void main(String[] args) {
+		Unidade a = new Unidade(8);
+		System.out.println( a.retornarValorPorExtenso()  );
+		
+		a.alterarValor(9);
+		System.out.println( a.retornarValorPorExtenso()  );
+	}
+}
--- TURMA_17_04_2017/7/aula_08_05_2017__07/src/br/com/turma_java/Unidade.java
+++ TURMA_17_04_2017/7/aula_08_05_2017__07/src/br/com/turma_java/Unidade.java
@@ -1,6 +1,36 @@
 package br.com.turma_java;
 
 public class Unidade {
-	private String[] unidades = {"Zero", "Um", "Dois", "Três", 
-			"Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove"};
+	private String[] unidades = { "Zero", "Um", "Dois", "Três", 
+			"Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove" };
+
+	private int valor;
+
+	protected Unidade() {
+		// valor = 0;
+		// alterarValor(0);
+		this(0);
+	}
+
+	public Unidade(int numero) {
+		// valor = numero;
+		alterarValor(numero);
+	}
+
+	public void alterarValor(int numero) {
+		valor = numero;
+	}
+
+	public String retornarValorPorExtenso() {
+		if (valor < 0 || valor > 9) {
+			return "";
+		}
+		
+		return unidades[valor];
+	}
 }
+
+
+
+
+
