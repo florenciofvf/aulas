5a8fdf9e7c8f2aa07fc405a8ecba9d137c718be1
Florêncio Vieira Filho <florenciovieira@gmail.com>
2017-05-08 19:37:27 -0300

    Construtores

--- TURMA_17_04_2017/7/aula_08_05_2017__07/src/br/com/turma_java/TesteUnidade.java
+++ TURMA_17_04_2017/7/aula_08_05_2017__07/src/br/com/turma_java/TesteUnidade.java
@@ -1,6 +1,7 @@
 package br.com.turma_java;
 
 public class TesteUnidade {
+	
 	public static void main(String[] args) {
 		Unidade a = new Unidade(8);
 		System.out.println( a.retornarValorPorExtenso()  );
@@ -8,4 +9,5 @@ public class TesteUnidade {
 		a.alterarValor(9);
 		System.out.println( a.retornarValorPorExtenso()  );
 	}
+	
 }
