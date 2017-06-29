55cf3570bda799e0b5e538f650f84df5002b708a
Florêncio Vieira Filho <florenciovieira@gmail.com>
2017-06-28 22:38:06 -0300

    Final

---
+++ TURMA_17_04_2017/25/aula_28_06_2017_25/src/aula_28_06_2017_25/Main.java
@@ -0,0 +1,20 @@
+package aula_28_06_2017_25;
+
+public class Main {
+	
+	public static void main(String[] args) {
+//		int[] vls = {23, 78, 90};
+		
+//		teste(  new int[]{23, 78, 90}  );
+//		teste(  vls  );
+//		teste(  23, 78, 90, 89, 78, -3, 56  );
+	}
+
+	public static void teste(int... valores) {
+		System.out.println("A");
+	}
+
+	public static void teste(short a) {
+		System.out.println("B");
+	}
+}
