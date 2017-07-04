edf9c7186a2dff3a3d692e0a938717f80a81aa90
Florêncio Vieira Filho <florenciovieira@gmail.com>
2017-07-04 20:48:11 -0300

    Barra de Ações

--- TURMA_11_05_2017/7/aula_04_07_2017__07/BarraAcoes/app/src/main/java/turma_android/com/br/barraacoes/MainActivity.java
+++ TURMA_11_05_2017/7/aula_04_07_2017__07/BarraAcoes/app/src/main/java/turma_android/com/br/barraacoes/MainActivity.java
@@ -1,17 +1,33 @@
 package turma_android.com.br.barraacoes;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        //CONECTAR AMBOS OS COMPONENTES
+        toggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar, R.string.app_name, R.string.app_name);
+        toggle.syncState();
     }
 
     @Override
@@ -23,6 +39,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void mensagem(MenuItem item) {
         Toast.makeText(this, "Mensagem de Teste",
-                Toast.LENGTH_SHORT).show();
+                                    Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.itemConfig) {
+            Toast.makeText(this, "Configurações",
+                                    Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
